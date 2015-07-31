fca93598dfe550a239b85163f99b30cf75aa5409
Francis Brunelle <frabrunelle@gmail.com>
2015-07-31 01:32:12 -0400

    add date on receipt page

--- client/helpers/accounting.js
+++ client/helpers/accounting.js
@@ -1,11 +1,15 @@
-Template.registerHelper('formatNumber', function(number) {
-  return accounting.formatNumber(number, Session.get('counterCurrency.precision'));
-});
-
 Template.registerHelper('formatBaseCurrency', function (number) {
   return accounting.formatMoney(number, { symbol: Session.get('baseCurrency.code'), format: "%v %s", precision: Session.get('baseCurrency.precision') });
 });
 
 Template.registerHelper('formatCounterCurrency', function (number) {
   return accounting.formatMoney(number, { symbol: Session.get('counterCurrency.code'), format: "%v %s", precision: Session.get('counterCurrency.precision') });
+});
+
+Template.registerHelper('formatDate', function (date) {
+  return moment(date).format("dddd, MMMM Do YYYY")
+});
+
+Template.registerHelper('formatNumber', function (number) {
+  return accounting.formatNumber(number, Session.get('counterCurrency.precision'));
 });
