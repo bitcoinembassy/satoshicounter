65aeaa6feb7c8ccefc6903efcd355c9709631a62
Francis Brunelle <frabrunelle@gmail.com>
2015-08-20 11:37:45 -0400

    always display time in moment.calendar

--- client/subscriptions.js
+++ client/subscriptions.js
@@ -5,4 +5,15 @@ Meteor.startup(function () {
   Meteor.subscribe('paymentMethods');
 
   AutoForm.debug();
+
+  moment.locale('en', {
+    calendar : {
+        lastDay : '[Yesterday at] LT',
+        sameDay : '[Today at] LT',
+        nextDay : '[Tomorrow at] LT',
+        lastWeek : '[last] dddd [at] LT',
+        nextWeek : 'dddd [at] LT',
+        sameElse : 'L [at] LT'
+    }
+  });
 });
